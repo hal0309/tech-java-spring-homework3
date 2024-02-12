53d4f2101c0c68a6f8110eab98c37b9b394c3c59
y.watanabe <oomori.gohan39@gmail.com>
2024-02-12 16:18:09 +0900

    2024211課題再レビュー依頼のため(1.初期設定 ~4. 機能修正)

---
+++ src/main/java/com/example/homework3/controller/RamenController.java
@@ -0,0 +1,32 @@
+package com.example.homework3.controller;
+
+import com.example.homework3.model.Ramen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("ramen")
+public class RamenController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+    @GetMapping("/getRamen")
+    public List<Map<String, Object>> getRamen(){
+        String sql = "SELECT * FROM ramen_table";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+        return list;
+    }
+
+    @PostMapping("/insertRamen")
+    public String insertRamen(@RequestBody Ramen ramen){
+        String sql = "INSERT INTO ramen_table(name, price, place) VALUES('"+ramen.name()+"', "+ramen.price()+", '"+ramen.place()+"')";
+        jdbcTemplate.update(sql);
+        return "insert success";
+    }
+}
