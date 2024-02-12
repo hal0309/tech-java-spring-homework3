472e66449f8325beac7633002d95ff4e3930d030
y.watanabe <oomori.gohan39@gmail.com>
2024-02-12 16:18:09 +0900

    2024211課題レビュー依頼のため(1.初期設定 ~4. 機能修正)

--- src/main/java/com/example/homework3/controller/UserController.java
+++ src/main/java/com/example/homework3/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.homework3.controller;
 
+import com.example.homework3.model.Ramen;
 import com.example.homework3.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,6 +26,20 @@ public class UserController {
     @PostMapping("/insertUser")
     public String insertUser(@RequestBody User user){
         String sql = "INSERT INTO user_table(name, age) VALUES('"+user.name()+"', "+user.age()+")";
+        jdbcTemplate.update(sql);
+        return "insert success";
+    }
+
+    @GetMapping("/getRamen")
+    public List<Map<String, Object>> getRamen(){
+        String sql = "SELECT * FROM ramen_table";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
+        return list;
+    }
+
+    @PostMapping("/insertRamen")
+    public String insertRamen(@RequestBody Ramen ramen){
+        String sql = "INSERT INTO ramen_table(name, price, place) VALUES('"+ramen.name()+"', "+ramen.price()+", '"+ramen.place()+"')";
         jdbcTemplate.update(sql);
         return "insert success";
     }
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,9 +1,18 @@
-DROP TABLE IF EXISTS user_table;
+DROP TABLE IF EXISTS user_table,ramen_table;
 
 CREATE TABLE user_table
 (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
     age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE ramen_table
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    price INT,
+    place VARCHAR(100),
     PRIMARY KEY(id)
 );
