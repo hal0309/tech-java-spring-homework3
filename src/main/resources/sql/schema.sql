53dc484764dd428aefe2a7efdfc0b6f415a85777
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-02-15 23:47:06 +0900

    homework 20240211 ans

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,9 +1,16 @@
-DROP TABLE IF EXISTS user_table;
-
-CREATE TABLE user_table
+CREATE TABLE IF NOT EXISTS user_table
 (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
     age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS ramen_table
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    price INT,
+    place VARCHAR(100),
     PRIMARY KEY(id)
 );
